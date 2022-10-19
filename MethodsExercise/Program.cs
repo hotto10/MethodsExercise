6207d922f9ca70cbd70ade4326cece39102ce845
heather otto <heatherotto32@yahoo.com>
2022-10-19 10:02:04 -0500

    finished exercises

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var myName = Console.ReadLine();
+            Console.WriteLine("What is your middle name?");
+            var myMiddleName = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            var myColor = Console.ReadLine();
+            Console.WriteLine ("What is your favorite hobby?");
+            var myHobby = Console.ReadLine();
+            MyMethod(myName, myMiddleName, myColor, myHobby);
         }
+
+        static void MyMethod(string myName, string myMiddleName, string myColor, string myHobby)
+        {
+            var myStory = ($"This is a short sweet story about {myName}. ");
+            myStory = myStory + ($"They always wanted to be addressed as {myMiddleName} instead of their first name. ");
+            myStory = myStory + ($"Most of the time they got made fun of for liking {myColor} and for liking {myHobby}. ");
+            Console.WriteLine(myStory);
+        }
+            
     }
+
 }
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Class1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a random whole number...");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another random whole number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            MyAdd(num1, num2);
+            MySub(num1, num2);
+
+            Console.WriteLine();
+            Console.WriteLine("End of Test");
+        }
+
+        public static void MyAdd(int num1, int num2)
+        {
+            int result = (num1 + num2);
+            Console.WriteLine($"The sume of {num1} and {num2} is : {result}");
+            return;
+        }
+        public static void MySub(int num1, int num2)
+        {
+            int result = (num1 - num2);
+            Console.WriteLine($"The remainder of {num1} minus {num2} is : {result}");
+        }
+    }
+}
